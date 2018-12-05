bc211be1c637ba9ccdb1b8ca0d23ef5a72d22461
黃志仁 <tbsthitw@gmail.com>
2018-12-05 12:59:07 +0800

    remove unnecessary import

--- PlainTextDB.java
+++ PlainTextDB.java
@@ -1,8 +1,5 @@
-import java.io.*;
-import java.util.*;
-import java.util.stream.Stream;
 import java.nio.file.*;
-import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 public class PlainTextDB extends DAO {
 	private Path filePath;
@@ -20,7 +17,7 @@ public class PlainTextDB extends DAO {
 	public void put(String name, String number) {
 		try {
 			Files.write(filePath, (name + "," + number + "\n").getBytes(), StandardOpenOption.APPEND);
-		} catch (IOException e) {
+		} catch (Exception e) {
 		}
 	}
 
@@ -31,7 +28,7 @@ public class PlainTextDB extends DAO {
 						.filter(data -> data.startsWith(name + CSV_SEPERATOR))
 						.findFirst().get()
 						.split(CSV_SEPERATOR)[1];
-		} catch (IOException e) {
+		} catch (Exception e) {
 		}
 		return null;
 	}
@@ -40,8 +37,7 @@ public class PlainTextDB extends DAO {
 	public void edit(String name, String number) {
 		try(Stream<String> input = Files.lines(filePath)) {
 			StringBuilder content = new StringBuilder();
-			input
-				.map(s -> s.startsWith(name + CSV_SEPERATOR) ? (name + "," + number) : s)
+			input.map(s -> s.startsWith(name + CSV_SEPERATOR) ? (name + "," + number) : s)
 				.forEachOrdered(s -> content.append(s + "\n"));
 			
 			Files.write(filePath, content.toString().getBytes());
@@ -52,8 +48,7 @@ public class PlainTextDB extends DAO {
 	public void delete(String name) {
 		try(Stream<String> input = Files.lines(filePath)) {
 			StringBuilder content = new StringBuilder();
-			input
-				.filter(s -> !s.startsWith(name + CSV_SEPERATOR))
+			input.filter(s -> !s.startsWith(name + CSV_SEPERATOR))
 				.forEachOrdered(s -> content.append(s + "\n"));
 
 			Files.write(filePath, content.toString().getBytes());
